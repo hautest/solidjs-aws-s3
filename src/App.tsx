0eeee0bb2f0a27b548dacfaf15d016e7bb3b109a
hautest <dos07008@naver.com>
2025-02-02 22:37:18 +0900

    배포테스트4

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ const App = () => {
   return (
     <div class="content">
       <h1>Solidjs를 aws s3로 배포해보자</h1>
-      <div>배포 테스트3</div>
+      <div>배포 테스트4</div>
       <span>{count()}</span>
       <button onClick={() => setCount(count() + 1)}>Increment</button>
     </div>
